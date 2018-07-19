da23b94f4b7d816223042449dfbcb2b46f5cd146
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-07-19 11:11:00 +0200

    Use gpg command for signing

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,5 +3,5 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
     openssl aes-256-cbc -K $encrypted_a4e132a4b597_key -iv $encrypted_a4e132a4b597_iv \
         -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
     gpg --fast-import travis/codesigning.asc
-  ./gradlew publish closeAndReleaseRepository -PossrhUsername=${OSSRH_JIRA_USERNAME} -PossrhPassword=${OSSRH_JIRA_PASSWORD} -Psigning.keyId=${GPG_KEY_NAME} -Psigning.password=${GPG_PASSPHRASE} -Psigning.secretKeyRingFile=codesigning.gpg
+  ./gradlew publish closeAndReleaseRepository -PossrhUsername=${OSSRH_JIRA_USERNAME} -PossrhPassword=${OSSRH_JIRA_PASSWORD}
 fi
