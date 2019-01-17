248a9677d03ac9451467aa8cd73898acf6b470c8
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2019-01-17 12:30:30 +0100

    Downgrade nexus-staging plugin

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,5 +3,5 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
     openssl aes-256-cbc -K $encrypted_a4e132a4b597_key -iv $encrypted_a4e132a4b597_iv \
         -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
     gpg --fast-import travis/codesigning.asc
-  ./gradlew publish --info --stacktrace closeAndReleaseRepository -PossrhUsername=${OSSRH_JIRA_USERNAME} -PossrhPassword=${OSSRH_JIRA_PASSWORD} -Psigning.gnupg.passphrase=${GPG_PASSPHRASE} -Psigning.gnupg.keyName=49190908 -Psigning.gnupg.useLegacyGpg=true
+  ./gradlew publish --info closeAndReleaseRepository -PossrhUsername=${OSSRH_JIRA_USERNAME} -PossrhPassword=${OSSRH_JIRA_PASSWORD} -Psigning.gnupg.passphrase=${GPG_PASSPHRASE} -Psigning.gnupg.keyName=49190908 -Psigning.gnupg.useLegacyGpg=true
 fi
