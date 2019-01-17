16d0985f9ae9ed71e410e51ea5b6985720e64933
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2019-01-17 15:11:45 +0100

    Convert build script to Kotlin DSL, attempt clean up Travis configuration

--- travis/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_a4e132a4b597_key -iv $encrypted_a4e132a4b597_iv \
-        -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
-    gpg --fast-import travis/codesigning.asc
-  ./gradlew publish getStagingProfile closeAndReleaseRepository --info -PossrhUsername=${OSSRH_JIRA_USERNAME} -PossrhPassword=${OSSRH_JIRA_PASSWORD} -Psigning.gnupg.passphrase=${GPG_PASSPHRASE} -Psigning.gnupg.keyName=49190908 -Psigning.gnupg.useLegacyGpg=true
-fi
